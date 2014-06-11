1e51f6fb54b1060a806ce98038b134bf297a60d2
gslowikowski <gslowikowski@0a1f0b36-0e7f-8a7b-3831-50ad4cbb12b1>
2014-06-11 13:49:20 +0000

    #40 - Mavenize Play! Deadbolt Module v.2.3.0-RC1 - Deadbolt "2.3.0-RC1" scripts and poms added.

---
+++ tools/scripts/sources/deadbolt-java/2.3.0-RC1/prepare.bat
@@ -0,0 +1,5 @@
+call wget http://schaloner.github.io/releases/be.objectify/deadbolt-java_2.11/2.3.0-RC1/ivys/ivy.xml
+call wget http://schaloner.github.io/releases/be.objectify/deadbolt-java_2.11/2.3.0-RC1/poms/deadbolt-java_2.11.pom
+call wget http://schaloner.github.io/releases/be.objectify/deadbolt-java_2.11/2.3.0-RC1/jars/deadbolt-java_2.11.jar
+call wget http://schaloner.github.io/releases/be.objectify/deadbolt-java_2.11/2.3.0-RC1/srcs/deadbolt-java_2.11-sources.jar
+call wget http://schaloner.github.io/releases/be.objectify/deadbolt-java_2.11/2.3.0-RC1/docs/deadbolt-java_2.11-javadoc.jar
